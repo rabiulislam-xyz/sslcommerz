19a4c21c58b1118219b3b18d694dd2aed928d0bf
Rabiul Islam <rabiulislam993@live.com>
2019-06-17 17:27:48 +0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='sslcommerze',  
+     version='0.1',
+     scripts=['sslc'] ,
+     author="Rabiul Islam",
+     author_email="rabiulislam993@live.com",
+     description="Payment integretion for SSLCOMMERZ",
+     long_description=long_description,
+   long_description_content_type="text/markdown",
+     url="https://github.com/rabiulislam993/sslcommerz",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
