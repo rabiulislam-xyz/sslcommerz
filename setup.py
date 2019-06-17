35c68368f50e892cbd8ea9be01634ae865f767e1
Rabiul Islam <rabiulislam993@live.com>
2019-06-17 17:43:32 +0600

    fixed sslcommerz name issue

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     name='sslcommerze',  
+     name='sslcommerz',  
      version='0.1',
      scripts=['sslc'] ,
      author="Rabiul Islam",
